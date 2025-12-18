709a13a1c59407454b2aad561c96a76c4be6c3ac
Plesionite <aleksejyurjev@gmail.com>
2025-12-18 19:45:53 +0300

    Revert "добавил правила линтера, преттир, алиасы"
    This reverts commit 3ac203a71412f17f0ee6b21b8d21cadc54602f90.

--- src/app/App.tsx
+++
@@ -1,9 +0,0 @@
-import '@app/App.css';
-
-export const App = () => {
-  return (
-    <>
-      <h1>Vite + React</h1>
-    </>
-  );
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
-import react from '@vitejs/plugin-react';
-import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, 'src') },
-      { find: '@app', replacement: path.resolve(__dirname, 'src/app') },
-      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
-      { find: '@modules', replacement: path.resolve(__dirname, 'src/modules') },
-      { find: '@shared', replacement: path.resolve(__dirname, 'src/shared') }
-    ]
-  }
-});
+})
