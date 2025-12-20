c46a43af59281222d637272afba1a94684f0dda7
Plesionite <aleksejyurjev@gmail.com>
2025-12-20 12:20:57 +0300

    изменил метод импорта свг

--- src/assets/icons/components/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as HeaderLogo } from './HeaderLogo';
-export { default as LoaderBig } from './LoaderBig';
-export { default as LoaderSmall } from './LoaderSmall';
-export { default as Moon } from './Moon';
-export { default as Sun } from './Sun';
