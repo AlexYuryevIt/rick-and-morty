3c88ab569a625da288df0685324e0dd6b787d9bd
Plesionite <aleksejyurjev@gmail.com>
2025-12-28 00:04:20 +0300

    feat: добавил компонент инпута

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -6,5 +6,6 @@ export { default as HeaderLogo } from './icons/header-logo.svg?react';
 export { default as LoaderBig } from './icons/loader-big.svg?react';
 export { default as LoaderSmall } from './icons/loader-small.svg?react';
 export { default as Moon } from './icons/moon.svg?react';
+export { default as Search } from './icons/search.svg?react';
 export { default as Sun } from './icons/sun.svg?react';
 export { default as banner } from './images/banner.png';
